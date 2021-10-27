c1f0d3a85d1528cf5a9479473492cce1ee872ade
Irina Romjalis <iromjalis@gmail.com>
2021-10-27 22:24:21 +0300

    Update phonebook-actions.js.js

--- src/redux/phonebook/phonebook-actions.js.js
+++ src/redux/phonebook/phonebook-actions.js.js
@@ -1,14 +1,28 @@
 import { createAction } from "@reduxjs/toolkit";
-import shortid from "shortid";
 
-const addContact = createAction("contacts/add", (name, number) => ({
-  payload: {
-    id: shortid.generate(),
-    name,
-    number,
-  },
-}));
-const deleteContact = createAction("phonebook/delete");
+const addContactRequest = createAction("contacts/addContactRequest");
+const addContactSuccess = createAction("contacts/addContactSuccess");
+const addContactError = createAction("contacts/addContactError");
+
+const deleteContactRequest = createAction("contacts/deleteContactRequest");
+const deleteContactSuccess = createAction("contacts/deleteContactSuccess");
+const deleteContactError = createAction("contacts/deleteContactError");
+
+const getContactRequest = createAction("contacts/getContactRequest");
+const getContactSuccess = createAction("contacts/getContactSuccess");
+const getContactError = createAction("contacts/getContactError");
+
 const changeFilter = createAction("phonebook/changeFilter");
 
-export { addContact, deleteContact, changeFilter };
+export {
+  addContactRequest,
+  addContactSuccess,
+  addContactError,
+  deleteContactError,
+  deleteContactSuccess,
+  deleteContactRequest,
+  getContactError,
+  getContactSuccess,
+  getContactRequest,
+  changeFilter,
+};
